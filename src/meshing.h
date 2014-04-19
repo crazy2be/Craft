8450ca0a6bcea85b689326e693927361df5dba95
crazy2be <code@jmcgirr.com>
2014-04-19 11:12:50 -0400

    Move some meshing code into seperate file.

---
+++ src/meshing.h
@@ -0,0 +1,13 @@
+typedef struct {
+    int p;
+    int q;
+    int load;
+    Map *block_maps[3][3];
+    Map *light_maps[3][3];
+    int miny;
+    int maxy;
+    int faces;
+    GLfloat *data;
+} WorkerItem;
+
+void compute_chunk(WorkerItem *item);
